6780c21dca3840215e6b9db1e304d8f24536006c
Eduardo Ferrer <duduferrer7@gmail.com>
2024-08-20 23:17:22 -0300

    feat: add menu as sheet component

--- facca-mp/src/components/ui/header.tsx
+++ facca-mp/src/components/ui/header.tsx
@@ -1,13 +1,26 @@
-import { MenuIcon, ShoppingBasketIcon } from "lucide-react";
+import { LogInIcon, MenuIcon, ShoppingBasketIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex justify-between p-5 items-center">
-      <Button size={"icon"} className="rounded" variant={"ghost"}>
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger>
+          <Button size={"icon"} className="rounded" variant={"ghost"}>
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={"left"}>
+          <SheetHeader>Menu</SheetHeader>
+          <div className="mt-4 flex flex-col">
+            <Button variant={"outline"}>
+              <LogInIcon className="mr-2" /> Entrar
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
       <h1 className="font-black text-2xl"> FACCA </h1>
       <Button size={"icon"} className="rounded" variant={"ghost"}>
         <ShoppingBasketIcon />

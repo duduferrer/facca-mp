fb2be10d761db98d83083a7b21203ac84bc05812
Eduardo Ferrer <duduferrer7@gmail.com>
2024-09-06 01:30:15 -0300

    refactor: add menucontent, remove content from header

--- facca-mp/src/components/header.tsx
+++ facca-mp/src/components/header.tsx
@@ -1,39 +1,11 @@
 "use client";
-import {
-  LogInIcon,
-  LogOutIcon,
-  MenuIcon,
-  MoonIcon,
-  ShoppingBasketIcon,
-} from "lucide-react";
+import { MenuIcon, ShoppingBasketIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { useTheme } from "next-themes";
-import { Switch } from "./ui/switch";
-import { useEffect, useState } from "react";
+import { Sheet, SheetTrigger } from "./ui/sheet";
+import MenuContent from "./menuContent";
 
 const Header = () => {
-  const handleLoginClick = async () => {
-    await signIn();
-  };
-  const handleLogoutClick = async () => {
-    await signOut();
-  };
-  const { status, data } = useSession();
-  const { setTheme, theme } = useTheme();
-  const [isChecked, setChecked] = useState(false);
-  useEffect(() => {
-    theme == "dark" ? setChecked(true) : setChecked(false);
-  }, []);
-  useEffect(() => {
-    isChecked ? setTheme("dark") : setTheme("light");
-    console.log(isChecked);
-    localStorage.setItem("theme-isChecked", isChecked.toString());
-  }, [isChecked]);
-
   return (
     <Card className="flex justify-between p-5 items-center">
       <Sheet>
@@ -42,56 +14,7 @@ const Header = () => {
             <MenuIcon />
           </Button>
         </SheetTrigger>
-        <SheetContent side={"left"}>
-          <div className="flex items-center justify-between mb-12 mt-5">
-            <SheetHeader className="text-2xl font-bold">Menu</SheetHeader>
-            <div className="flex gap-2 items-center">
-              <Switch onCheckedChange={setChecked} checked={isChecked} />
-              <MoonIcon size={20} />
-            </div>
-          </div>
-          <div className="mt-4 flex flex-col">
-            <div className="mb-4">
-              {status == "unauthenticated" ? (
-                <div>
-                  <p className="font-bold">Bem-vindo! </p>
-                  <p>
-                    Clique em <span className="font-semibold">Entrar</span> para
-                    fazer login.{" "}
-                  </p>
-                </div>
-              ) : (
-                <div className="flex items-center gap-4">
-                  <Avatar>
-                    <AvatarImage
-                      src={data?.user?.image?.toString()}
-                      alt={data?.user?.name?.toString().split(" ")[0]}
-                    />
-                    <AvatarFallback>
-                      {data?.user?.name?.toString().slice(0, 2).toUpperCase()}
-                    </AvatarFallback>
-                  </Avatar>
-                  <p>
-                    Olá,{" "}
-                    <span className="font-semibold">
-                      {data?.user?.name?.toString().split(" ")[0].toUpperCase()}
-                      !
-                    </span>
-                  </p>
-                </div>
-              )}
-            </div>
-            {status == "unauthenticated" ? (
-              <Button variant={"outline"} onClick={handleLoginClick}>
-                <LogInIcon className="mr-2" /> Entrar
-              </Button>
-            ) : (
-              <Button variant={"outline"} onClick={handleLogoutClick}>
-                <LogOutIcon className="mr-2" /> Sair
-              </Button>
-            )}
-          </div>
-        </SheetContent>
+        <MenuContent />
       </Sheet>
       <h1 className="font-black text-2xl"> FACCA </h1>
       <Button size={"icon"} className="rounded" variant={"ghost"}>
---
+++ facca-mp/src/components/menuContent.tsx
@@ -0,0 +1,82 @@
+import { LogInIcon, LogOutIcon, MoonIcon } from "lucide-react";
+import { SheetContent, SheetHeader } from "./ui/sheet";
+import { Switch } from "./ui/switch";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+
+const MenuContent = () => {
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+  const { status, data } = useSession();
+  const { setTheme, theme } = useTheme();
+  const [isChecked, setChecked] = useState(false);
+  useEffect(() => {
+    theme == "dark" ? setChecked(true) : setChecked(false);
+  }, []);
+  useEffect(() => {
+    isChecked ? setTheme("dark") : setTheme("light");
+    console.log(isChecked);
+    localStorage.setItem("theme-isChecked", isChecked.toString());
+  }, [isChecked]);
+
+  return (
+    <SheetContent side={"left"}>
+      <div className="flex items-center justify-between mb-12 mt-5">
+        <SheetHeader className="text-2xl font-bold">Menu</SheetHeader>
+        <div className="flex gap-2 items-center">
+          <Switch onCheckedChange={setChecked} checked={isChecked} />
+          <MoonIcon size={20} />
+        </div>
+      </div>
+      <div className="mt-4 flex flex-col">
+        <div className="mb-4">
+          {status == "unauthenticated" ? (
+            <div>
+              <p className="font-bold">Bem-vindo! </p>
+              <p>
+                Clique em <span className="font-semibold">Entrar</span> para
+                fazer login.{" "}
+              </p>
+            </div>
+          ) : (
+            <div className="flex items-center gap-4">
+              <Avatar>
+                <AvatarImage
+                  src={data?.user?.image?.toString()}
+                  alt={data?.user?.name?.toString().split(" ")[0]}
+                />
+                <AvatarFallback>
+                  {data?.user?.name?.toString().slice(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+              <p>
+                Olá,{" "}
+                <span className="font-semibold">
+                  {data?.user?.name?.toString().split(" ")[0].toUpperCase()}!
+                </span>
+              </p>
+            </div>
+          )}
+        </div>
+        {status == "unauthenticated" ? (
+          <Button variant={"outline"} onClick={handleLoginClick}>
+            <LogInIcon className="mr-2" /> Entrar
+          </Button>
+        ) : (
+          <Button variant={"outline"} onClick={handleLogoutClick}>
+            <LogOutIcon className="mr-2" /> Sair
+          </Button>
+        )}
+      </div>
+    </SheetContent>
+  );
+};
+
+export default MenuContent;

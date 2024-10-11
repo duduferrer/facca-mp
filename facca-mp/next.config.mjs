7fc294c2cf72e79c850102da5cb6712f4924e5fa
Eduardo Ferrer <duduferrer7@gmail.com>
2024-10-11 03:00:35 -0300

    chore: add images remote patterns to config file

--- facca-mp/next.config.mjs
+++ facca-mp/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-cosmos.bluesoft.com.br",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;

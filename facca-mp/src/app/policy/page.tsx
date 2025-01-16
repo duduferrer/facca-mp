913e665b0c759111f02339c9df9a7b073ab470f3
Eduardo Ferrer <duduferrer7@gmail.com>
2025-01-16 00:50:24 -0300

    feat: add service terms and privacy policy

---
+++ facca-mp/src/app/policy/page.tsx
@@ -0,0 +1,37 @@
+<p>
+  Política de Privacidade 1. Introdução A Facca, acessível em
+  https://facca.app.br, valoriza a privacidade de seus usuários e está
+  comprometida em proteger as informações pessoais coletadas. Esta Política de
+  Privacidade descreve como coletamos, usamos, armazenamos e compartilhamos seus
+  dados. 2. Informações que Coletamos Podemos coletar as seguintes informações:
+  Dados Pessoais: Nome, endereço de e-mail e foto de perfil do Google. Dados de
+  Uso: Informações sobre como você interage com nosso site, como endereço IP,
+  tipo de navegador, páginas acessadas e tempo de visita. Cookies: Pequenos
+  arquivos armazenados em seu dispositivo para melhorar a experiência do
+  usuário. 3. Como Usamos Suas Informações As informações coletadas podem ser
+  usadas para: Criar e gerenciar seu perfil no site. Fornecer, operar e melhorar
+  nossos serviços. Personalizar sua experiência no site. Cumprir obrigações
+  legais e proteger nossos direitos. 4. Compartilhamento de Dados Não vendemos,
+  alugamos ou compartilhamos suas informações pessoais com terceiros, exceto nas
+  seguintes situações: Provedores de Serviços: Empresas terceirizadas que nos
+  ajudam a operar o site ou fornecer serviços em nosso nome, como: Hospedagem:
+  Vercel Banco de dados: NeonDB Autenticação: Google Authentication Requisitos
+  Legais: Quando exigido por lei ou para proteger nossos direitos. Consentimento
+  do Usuário: Quando você nos autorizar explicitamente. 5. Proteção de Dados
+  Adotamos medidas técnicas e organizacionais adequadas para proteger suas
+  informações contra acesso não autorizado, perda, alteração ou divulgação. 6.
+  Retenção de Dados Reteremos suas informações pessoais apenas pelo tempo
+  necessário para cumprir os propósitos descritos nesta Política, salvo se um
+  período maior for exigido por lei. 7. Direitos do Usuário Você tem os
+  seguintes direitos em relação às suas informações pessoais: Acessar, corrigir
+  ou excluir seus dados pessoais. Revogar seu consentimento para o uso de seus
+  dados. Solicitar a portabilidade de seus dados. Para exercer seus direitos,
+  entre em contato conosco em duduferrer7@gmail.com. 8. Cookies e Tecnologias
+  Semelhantes Usamos cookies para melhorar sua experiência no site. Você pode
+  gerenciar as configurações de cookies no seu navegador. 9. Alterações nesta
+  Política de Privacidade Reservamo-nos o direito de atualizar esta Política de
+  Privacidade a qualquer momento. Quaisquer alterações serão publicadas nesta
+  página com a data de revisão atualizada. 10. Contato Se você tiver dúvidas ou
+  preocupações sobre esta Política de Privacidade, entre em contato conosco em:
+  E-mail: duduferrer7@gmail.com
+</p>;

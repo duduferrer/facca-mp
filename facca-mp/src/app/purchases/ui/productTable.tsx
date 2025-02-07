b7811f64ec5368589f4cc344a413f68a78934726
Eduardo Ferrer <duduferrer7@gmail.com>
2025-02-06 23:20:49 -0300

    feat: add final price to productOrder table

--- facca-mp/src/app/purchases/ui/productTable.tsx
+++ facca-mp/src/app/purchases/ui/productTable.tsx
@@ -16,6 +16,7 @@ const ProductTable = ({ order }: { order: TableOrder }) => {
           <TableHead className="w-16"></TableHead>
           <TableHead>Produto</TableHead>
           <TableHead className="text-center">Preço</TableHead>
+          <TableHead className="text-center">Preço Final por Item</TableHead>
           <TableHead className="text-center">Quantidade</TableHead>
           <TableHead className="text-right">Total</TableHead>
         </TableRow>
@@ -25,6 +26,7 @@ const ProductTable = ({ order }: { order: TableOrder }) => {
           let name = product.name;
           let price = product.price;
           let quantity = product.quantity;
+          let finalPrice = price - (price * Number(process.env.NEXT_PUBLIC_DISCOUNT_PERCENTAGE))
           return (
             <TableRow key={Math.floor(Math.random())*32}>
               <TableCell></TableCell>
@@ -32,9 +34,10 @@ const ProductTable = ({ order }: { order: TableOrder }) => {
                 {name}
               </TableCell>
               <TableCell className="text-center">{BRL.format(price)}</TableCell>
+              <TableCell className="text-center">{BRL.format(finalPrice)}</TableCell>
               <TableCell className="text-center">{quantity}</TableCell>
               <TableCell className="text-right">
-                {BRL.format(quantity * price)}
+                {BRL.format(quantity * finalPrice)}
               </TableCell>
             </TableRow>
           );

5dce033e7629ee529ee7a86255bb7995b1480f55
Eduardo Ferrer <smingolera@gmail.com>
2026-03-15 01:14:51 -0300

    feat: implement pagination, 2 items per page

--- facca-mp/src/app/(home)/homeClient.tsx
+++ facca-mp/src/app/(home)/homeClient.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Products from "../../components/productsList";
 import SearchBar from "@/components/searchBar";
@@ -6,37 +6,41 @@ import { useEffect, useMemo, useState } from "react";
 import { Product } from "@prisma/client";
 import { useToast } from "@/hooks/use-toast";
 
-
 type Props = {
-    products: Product[];
-    children: React.ReactNode;
-    categoryName: string;
-}
+  products: Product[];
+  children: React.ReactNode;
+  categoryName: string;
+};
 
 export const dynamic = "force-dynamic";
-export default function HomeClient({products, children, categoryName}: Props) {
+export default function HomeClient({
+  products,
+  children,
+  categoryName,
+}: Props) {
   const [searchTerm, setSearchTerm] = useState("");
-  const filteredProducts = useMemo(()=>{
-    return products.filter(product=> product.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  const filteredProducts = useMemo(() => {
+    return products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
   }, [searchTerm, products]);
-  useEffect(()=>{
-    localStorage.getItem("update")
-  },[]);
+  useEffect(() => {
+    localStorage.getItem("update");
+  }, []);
   return (
     <>
       <div className="px-4 pt-20">
         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <div>
-            {children}
-        </div>
+        <div>{children}</div>
         <div className="mt-6 p-3 max-w-full block">
           <h3 className="text-center font-bold mb-2">{categoryName}</h3>
           <div className="flex justify-center">
             <Products products={filteredProducts} />
           </div>
+          <div>Paginas</div>
         </div>
       </div>
     </>
   );
 }
-  //TODO adicionar categoria de favoritos
+//TODO adicionar categoria de favoritos
--- facca-mp/src/app/utils/db/filterProducts.ts
+++ facca-mp/src/app/utils/db/filterProducts.ts
@@ -3,34 +3,38 @@ import { db } from "@/lib/prisma";
 /**
  * Returns product list filtered by category
  * @param category - bebidas|salgados|doces|all
+ * @param page - pagina atual
+ * @param limit - numero de itens por pagina
  * @returns product list filtered by category
  */
 
-const productsFiltered = async (category: CategoryType) => {
-  if (category == "all") {
-    const products = await db.product.findMany({
-      where: {
+const productsFiltered = async (
+  category: CategoryType,
+  page: number = 1,
+  limit: number = 2,
+) => {
+  const skip = (page - 1) * limit;
+  let whereClause: any = { isVisible: true };
+  if (category != "all") {
+    whereClause = {
+      AND: {
+        category: {
+          slug: {
+            equals: category,
+          },
+        },
         isVisible: true,
       },
-    });
-    return products;
-  } else {
-    const products = await db.product.findMany({
-      orderBy: {
-        name: "asc",
-      },
-      where: {
-        AND: {
-          category: {
-            slug: {
-              equals: category,
-            },
-          },
-          isVisible: true,
-        },
-      },
-    });
-    return products;
+    };
   }
+  const products = await db.product.findMany({
+    orderBy: {
+      name: "asc",
+    },
+    where: whereClause,
+    take: limit,
+    skip: skip,
+  });
+  return products;
 };
 export default productsFiltered;
